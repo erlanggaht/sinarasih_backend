a50d6d24498b4561206984f460669ab6461b4a98
erlanggaht <erlanggahidayat.md@gmail.com>
2023-06-23 18:23:17 +0700

    first commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,6 @@ class Auth {
         
        const headers = req.headers
        const Authorization_Bearer = headers.authorization.split(' ')[1]
-       if(!Authorization_Bearer) return res.status(401).send('No Authorization')
 
        const verify = jwt.verify(Authorization_Bearer,process.env.TOKEN_RAHASIA,(err,decoded) => {
         if(err) return res.status(401).send('No Authorization')
@@ -14,6 +13,8 @@ class Auth {
             next()  
         }
        })
+
+      return res.status(401).send('No Authorization')
 
        
     }
