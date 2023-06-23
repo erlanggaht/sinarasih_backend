a69496b50e02f0371893f7ee596e224e96e64731
erlanggaht <erlanggahidayat.md@gmail.com>
2023-06-24 05:17:47 +0700

    ok

--- middleware/setcookie.js
+++ middleware/setcookie.js
@@ -1,7 +1,15 @@
 import cookieSession from "cookie-session"
 
 function setCookie(req,res,next){
+    
+    res.setHeader("Access-Control-Allow-Origin", 'https://sinarasih.vercel.app/');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
+  
+    next();
+    
     res.cookie('erlangga','dsjdhjshd')
+    
 }
 
 export default setCookie
